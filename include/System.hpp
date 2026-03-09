ceae0f4a4878e1932ced00a37eea03cb4b2ed9a9
Tomasz Mazur <toptomek05@gmail.com>
2026-03-09 02:34:43 +0100

    zrefaktoryzowalem strukture glownej klasy i wyprowadzilem odpowiednie algorytmy (stary zeszyt do rurek) (trzeba je tylko wdrozyc)

--- include/System.hpp
+++ include/System.hpp
@@ -7,30 +7,38 @@
 #include <boost/numeric/odeint.hpp>
 #include <boost/numeric/odeint/external/eigen/eigen.hpp>
 #include <Eigen/Dense>
+#include "ChebFunc.hpp"
+
+#include "Settings.hpp"
 
 class System
 {
     public:
         System(Eigen::VectorXd b_coeffs, Eigen::VectorXd a_coeffs);
-        enum ForcingFunctionType
-        {
-            STEP,
-            IMPULSE
-        };
-        void set_forcing_function(ForcingFunctionType forcing_function_type);
-        void set_forcing_function(const double* new_x_ptr_);
-        void set_forcing_function(std::function<double(double)> forcing_function);
 
-        void do_step();
+        using FuncType = std::variant<std::function<double(void)>, std::function<double(double)>>;
+
+        Eigen::VectorXd step_response(const Eigen::VectorXd& t);
+        Eigen::VectorXd impulse_response(const Eigen::VectorXd& t);
+
+        void set_forcing_func(FuncType func);
+        std::pair<double, double> do_RK4_step();
 
     private:
-        void operator()(const Eigen::VectorXd& Z, Eigen::VectorXd& dZdt, double dt);
-        struct TransferFunction
+        void operator()(const Eigen::VectorXd& Z, Eigen::VectorXd& dZdt, double t);
+
+        struct TransferFunc
         {
             Eigen::VectorXd b_coeffs;
             Eigen::VectorXd a_coeffs;
+            Eigen::MatrixXd M;
         } tf_;
-        const double* x_ptr_;
+
+        struct ForcingFunc
+        {
+            FuncType func;
+            double get_x(double t);
+        } forcing_func_;
 };
 
 #endif
--- src/System.cpp
+++ src/System.cpp
@@ -8,30 +8,76 @@ System::System(
 ) : 
     tf_{b_coeffs, a_coeffs}
 {
+    int m = tf_.b_coeffs.size()-1;
+    int n = tf_.a_coeffs.size()-1;
+    if ( m > n )
+    { throw std::runtime_error("m nie moze byc wieksze od n"); }
 
+    // tf_.b = Eigen::VectorXd::Zero(m+1);
+    // tf_.b.segment(0, m+1) = tf_.b_coeffs;
+
+    tf_.M = Eigen::MatrixXd::Zero(n, n);
+    tf_.M.block(1, 1, n-1, n-1) = Eigen::MatrixXd::Identity(n-1, n-1);
+    tf_.M.row(n-1) = tf_.a_coeffs.segment(0, n)*(-1/tf_.a_coeffs[n]);
 }
 
-void System::set_forcing_function(System::ForcingFunctionType forcing_function_type)
+
+Eigen::VectorXd System::step_response(const Eigen::VectorXd& t)
 {
 
 }
 
-void System::set_forcing_function(const double* new_x_ptr_)
+Eigen::VectorXd System::impulse_response(const Eigen::VectorXd& t)
 {
 
 }
 
-void System::set_forcing_function(std::function<double(double)> forcing_function)
+void System::set_forcing_func(FuncType func)
+{
+    int N = 16;
+    int m = tf_.b_coeffs.size() - 1;
+    if ( m < N-1 )
+    {
+        double L = 0.001;
+        std::pair<double, double> cheb_range{0.0, L};
+
+        Eigen::VectorXd cheb_nodes = ChebNodes(N, cheb_range.first, cheb_range.second);
+        Eigen::VectorXd cheb_vals = cheb_nodes.unaryExpr(func);
+        Eigen::VectorXd cheb_coeffs = ChebCoeffs(cheb_vals);
+
+        Eigen::MatrixXd X_derivatives{m};
+        Eigen::MatrixXd D = DifferentiationOperator(N); 
+
+        for ( int i = 0; i < m; i++ )  
+        { 
+            X_derivatives[i] = Clenshaw(cheb_coeffs, cheb_range.first);
+            cheb_coeffs = D*cheb_coeffs;
+        }
+
+
+    }
+    else 
+    { throw std::runtime_error("trzeba dodac mozliwosc rozniczkowania wiecej niz 16 razy"); }
+
+
+}
+
+std::pair<double, double> System::do_RK4_step()
 {
 
 }
 
-void System::do_step()
+void System::operator()(const Eigen::VectorXd& Z, Eigen::VectorXd& dZdt, double t)
 {
 
 }
 
-void System::operator()(const Eigen::VectorXd& Z, Eigen::VectorXd& dZdt, double dt)
+double System::ForcingFunc::get_x(double t)
 {
-    
+    std::visit(overloaded{
+        [&](std::function<double(void)> void_func)
+        { return void_func(); },
+        [&](std::function<double(double)> double_func)
+        { return double_func(t); }
+    }, func);
 }
